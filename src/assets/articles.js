2e650f925d9b8627492467d6bf6204a4d7b670c9
Brian Sunday <brians931@gmail.com>
2020-04-28 23:14:25 +0300

    editing

--- src/assets/articles.js
+++ src/assets/articles.js
@@ -5,38 +5,43 @@ export default [
     subtitle: "sorry arsenal could have played until sunrise",
     description:
       "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
-    imageUrl: "src/assets/images/details-1.jpeg",
+    imageUrl:
+      "https://i2-prod.football.london/arsenal-fc/news/article17966789.ece/ALTERNATES/s615/0_GettyImages-1211000503.jpg",
   },
   {
     id: "2",
-    title: "arsenal battered",
+    title: "demolorion at the etihad ",
     subtitle: "sorry arsenal could have played until sunrise",
     description:
       "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
-    imageUrl: "src/assets/images/details-2.jpeg",
+    imageUrl:
+      "https://www.ft.com/__origami/service/image/v2/images/raw/http%3A%2F%2Fcom.ft.imagepublish.upp-prod-us.s3.amazonaws.com%2F993abacc-6521-11ea-abcc-910c5b38d9ed?fit=scale-down&source=next&width=700",
   },
   {
     id: "3",
-    title: "arsenal battered",
+    title: "liverpool crowned champions",
     subtitle: "sorry arsenal could have played until sunrise",
     description:
       "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
-    imageUrl: "src/assets/images/details-3.jpeg",
+    imageUrl:
+      "https://www.dreamteamfc.com/c/wp-content/uploads/sites/4/2019/10/salahaa.jpg?strip=all&w=750&quality=100",
   },
   {
     id: "4",
-    title: "arsenal battered",
-    subtitle: "sorry arsenal could have played until sunrise",
+    title: "chelsea qualify for europe",
+    subtitle: "sorry arsenal",
     description:
       "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
-    imageUrl: "src/assets/images/details-4.jpeg",
+    imageUrl:
+      "https://img.bleacherreport.net/img/images/photos/003/856/436/hi-res-52ce61a0c2b9498cd597123e64774b13_crop_north.jpg?h=533&w=800&q=70&crop_x=center&crop_y=top",
   },
   {
     id: "5",
-    title: "arsenal battered",
+    title: "leicester collapse",
     subtitle: "sorry arsenal could have played until sunrise",
     description:
       "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
-    imageUrl: "src/assets/images/details-4.jpeg",
+    imageUrl:
+      "https://en.as.com/en/imagenes/2020/03/19/football/1584635631_043769_noticia_normal.jpg",
   },
 ];
