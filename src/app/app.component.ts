41495034ef95fda64218290f446ed501c0e92ed8
Brian Sunday <brians931@gmail.com>
2020-04-29 15:37:05 +0300

    add back button for the tickets page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
   constructor(
@@ -23,5 +23,9 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  onLogOut() {
+    console.log("logout");
   }
 }
--- src/app/team/team.page.ts
+++ src/app/team/team.page.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { SegmentChangeEventDetail } from "@ionic/core";
 
 @Component({
-  selector: 'app-team',
-  templateUrl: './team.page.html',
-  styleUrls: ['./team.page.scss'],
+  selector: "app-team",
+  templateUrl: "./team.page.html",
+  styleUrls: ["./team.page.scss"],
 })
 export class TeamPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onFilterContent(event: CustomEvent<SegmentChangeEventDetail>) {
+    console.log(event.detail);
   }
-
 }
