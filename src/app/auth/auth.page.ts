e4dc26cc93abfdfc1d9b626dd285fa83126ebfcb
Brian Sunday <brians931@gmail.com>
2020-04-29 16:03:31 +0300

    create a simplean auth guard

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../services/auth.service";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.page.html',
-  styleUrls: ['./auth.page.scss'],
+  selector: "app-auth",
+  templateUrl: "./auth.page.html",
+  styleUrls: ["./auth.page.scss"],
 })
 export class AuthPage implements OnInit {
+  constructor(private authSrvc: AuthService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onLogin() {
+    this.authSrvc.login();
   }
-
 }
---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import {
+  CanLoad,
+  Route,
+  UrlSegment,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "./auth.service";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthGuard implements CanLoad {
+  constructor(private authSrvc: AuthService, private router: Router) {}
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.authSrvc.currentUser) {
+      this.router.navigateByUrl("/auth");
+      return false;
+    }
+    return true;
+  }
+}
