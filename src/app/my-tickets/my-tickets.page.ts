143d5851d874ddcfad20af2d3c57a40d0d8f3046
Brian Sunday <brians931@gmail.com>
2020-04-30 18:43:16 +0300

    add swipeable sliding items

--- src/app/my-tickets/my-tickets.page.ts
+++ src/app/my-tickets/my-tickets.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Ticket } from "../models/article.model";
+import { TicketService } from "../services/ticket.service";
+import { IonItemSliding } from "@ionic/angular";
 
 @Component({
-  selector: 'app-my-tickets',
-  templateUrl: './my-tickets.page.html',
-  styleUrls: ['./my-tickets.page.scss'],
+  selector: "app-my-tickets",
+  templateUrl: "./my-tickets.page.html",
+  styleUrls: ["./my-tickets.page.scss"],
 })
 export class MyTicketsPage implements OnInit {
-
-  constructor() { }
+  tickets: Ticket[] = [];
+  constructor(private tcktSrvc: TicketService) {}
 
   ngOnInit() {
+    this.tickets = this.tcktSrvc.tickets;
   }
 
+  onTicketTrade(id: string, slidingItem: IonItemSliding) {
+    // close sliding item after clicking trade
+    slidingItem.close();
+    // action sheet will be added here
+    console.log(id);
+  }
 }
