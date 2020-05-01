203ff24867796e4e6efee48c003f4f969696ff52
Brian Sunday <brians931@gmail.com>
2020-05-01 14:09:37 +0300

    create a dummy auth page and a fullscreen loading controller

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "../services/auth.service";
+import { LoadingController } from "@ionic/angular";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-auth",
@@ -7,11 +9,32 @@ import { AuthService } from "../services/auth.service";
   styleUrls: ["./auth.page.scss"],
 })
 export class AuthPage implements OnInit {
-  constructor(private authSrvc: AuthService) {}
+  isLoading = false;
+  constructor(
+    private authSrvc: AuthService,
+    private loadingCtrl: LoadingController,
+    private router: Router
+  ) {}
 
   ngOnInit() {}
 
   onLogin() {
+    this.isLoading = true;
     this.authSrvc.login();
+    // mimicing full screen loading controller
+    this.loadingCtrl
+      .create({
+        keyboardClose: true,
+        spinner: "crescent",
+        message: "Authenticating...please wait",
+      })
+      .then((loadingEL) => {
+        loadingEL.present();
+        setTimeout(() => {
+          this.isLoading = false;
+          loadingEL.dismiss();
+          this.router.navigateByUrl("/home/tabs/massivefc");
+        }, 1500);
+      });
   }
 }
