eb53a01d1950225c9e009b371dfa247d1672affc
Brian Sunday <brians931@gmail.com>
2020-05-01 14:52:49 +0300

    add an actionsheet to confirm ticket trade

--- src/app/my-tickets/my-tickets.page.ts
+++ src/app/my-tickets/my-tickets.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Ticket } from "../models/article.model";
 import { TicketService } from "../services/ticket.service";
-import { IonItemSliding } from "@ionic/angular";
+import { IonItemSliding, ActionSheetController } from "@ionic/angular";
 
 @Component({
   selector: "app-my-tickets",
@@ -10,7 +10,10 @@ import { IonItemSliding } from "@ionic/angular";
 })
 export class MyTicketsPage implements OnInit {
   tickets: Ticket[] = [];
-  constructor(private tcktSrvc: TicketService) {}
+  constructor(
+    private tcktSrvc: TicketService,
+    private actionSheet: ActionSheetController
+  ) {}
 
   ngOnInit() {
     this.tickets = this.tcktSrvc.tickets;
@@ -20,6 +23,26 @@ export class MyTicketsPage implements OnInit {
     // close sliding item after clicking trade
     slidingItem.close();
     // action sheet will be added here
-    console.log(id);
+    this.actionSheet
+      .create({
+        header: "Are you sure you want to trade this ticket?",
+        buttons: [
+          {
+            text: "Yes i want to trade...",
+            handler: () => {
+              // function to trade will go here
+              console.log("done!!!!!!");
+              console.log(id);
+            },
+          },
+          {
+            text: "No, i was just messing around...",
+            role: "cancel",
+          },
+        ],
+      })
+      .then((actionSheetEL) => {
+        actionSheetEL.present();
+      });
   }
 }
