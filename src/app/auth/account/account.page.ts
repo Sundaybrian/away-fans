57d39bbc0ef0e79c85b85c29a7fcdd650c34343c
Brian Sunday <brians931@gmail.com>
2020-05-08 21:30:53 +0300

    add method to open general settings modal

--- src/app/auth/account/account.page.ts
+++ src/app/auth/account/account.page.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { GeneralDetailsModalPage } from "./general-details-modal/general-details-modal.page";
 
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.page.html',
-  styleUrls: ['./account.page.scss'],
+  selector: "app-account",
+  templateUrl: "./account.page.html",
+  styleUrls: ["./account.page.scss"],
 })
 export class AccountPage implements OnInit {
+  constructor(private modalCtrl: ModalController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  openModal() {
+    // open general details modal
+    this.modalCtrl
+      .create({
+        component: GeneralDetailsModalPage,
+        componentProps: {
+          user: {
+            id: 1,
+            name: "Amos Okumu",
+          },
+        },
+      })
+      .then((modalEL) => {
+        modalEL.present();
+      });
   }
-
 }
