2a3a17bfab3c82e22cf9db3be834358f8bebcef9
Brian Sunday <brians931@gmail.com>
2020-05-11 11:43:20 +0300

    update new offer page

--- src/app/travel/new-offer/new-offer.page.ts
+++ src/app/travel/new-offer/new-offer.page.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-new-offer',
-  templateUrl: './new-offer.page.html',
-  styleUrls: ['./new-offer.page.scss'],
+  selector: "app-new-offer",
+  templateUrl: "./new-offer.page.html",
+  styleUrls: ["./new-offer.page.scss"],
 })
 export class NewOfferPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onCreateOffer() {
+    console.log("created....");
   }
-
 }
