1aeb17d431bae3e41a383de0d7e419e7dd5bfd3b
Brian Sunday <brians931@gmail.com>
2020-05-13 12:40:23 +0300

    add booking actionsheet

--- src/app/travel/offer-booking/offer-booking.page.ts
+++ src/app/travel/offer-booking/offer-booking.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Travel } from "src/app/models/article.model";
 import { ActivatedRoute, Router } from "@angular/router";
 import { TravelService } from "src/app/services/travel.service";
-import { NavController } from "@ionic/angular";
+import { NavController, ActionSheetController } from "@ionic/angular";
 
 @Component({
   selector: "app-offer-booking",
@@ -15,7 +15,8 @@ export class OfferBookingPage implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private travelSrvc: TravelService,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private actionSheet: ActionSheetController
   ) {}
 
   ngOnInit() {
@@ -30,6 +31,30 @@ export class OfferBookingPage implements OnInit {
   }
 
   onBookVehicle() {
-    console.log("booked");
+    this.actionSheet
+      .create({
+        header: "Choose an action",
+        buttons: [
+          {
+            text: "Select Date",
+            handler: () => {
+              console.log("select date");
+            },
+          },
+          {
+            text: "Random Date",
+            handler: () => {
+              console.log("random date");
+            },
+          },
+          {
+            text: "Cancel",
+            role: "cancel",
+          },
+        ],
+      })
+      .then((actionEL) => {
+        actionEL.present();
+      });
   }
 }
