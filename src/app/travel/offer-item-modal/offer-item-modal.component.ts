885a24c1f69f028d6ea1df580321b85fc14bc3b4
Brian Sunday <brians931@gmail.com>
2020-05-13 13:09:19 +0300

    passed the vehicle obj to the modal

--- src/app/travel/offer-item-modal/offer-item-modal.component.ts
+++ src/app/travel/offer-item-modal/offer-item-modal.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Travel } from "src/app/models/article.model";
+import { ModalController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-offer-item-modal',
-  templateUrl: './offer-item-modal.component.html',
-  styleUrls: ['./offer-item-modal.component.scss'],
+  selector: "app-offer-item-modal",
+  templateUrl: "./offer-item-modal.component.html",
+  styleUrls: ["./offer-item-modal.component.scss"],
 })
 export class OfferItemModalComponent implements OnInit {
+  @Input() vehicle: Travel;
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) {}
 
   ngOnInit() {}
 
+  onCancel() {
+    this.modalCtrl.dismiss();
+  }
 }
