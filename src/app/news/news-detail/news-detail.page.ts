b3d2c6d5e2aa5b12e8c1f80890e1c591eded22af
Brian Sunday <brians931@gmail.com>
2020-05-31 20:26:13 +0300

    add news detail page

--- src/app/news/news-detail/news-detail.page.ts
+++ src/app/news/news-detail/news-detail.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Article } from "src/app/models/article.model";
+import { ActivatedRoute } from "@angular/router";
+import { NavController } from "@ionic/angular";
+import { HomeService } from "src/app/services/home.service";
 
 @Component({
-  selector: 'app-news-detail',
-  templateUrl: './news-detail.page.html',
-  styleUrls: ['./news-detail.page.scss'],
+  selector: "app-news-detail",
+  templateUrl: "./news-detail.page.html",
+  styleUrls: ["./news-detail.page.scss"],
 })
 export class NewsDetailPage implements OnInit {
+  article: Article;
 
-  constructor() { }
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private navCtrl: NavController,
+    private homeSrvc: HomeService
+  ) {}
 
   ngOnInit() {
+    // get id of new detail
+    this.activateRoute.paramMap.subscribe((paraMap) => {
+      if (!paraMap.has("id")) {
+        this.navCtrl.navigateBack("/home/tabs/news");
+        return;
+      }
+      // get article
+      this.article = this.homeSrvc.getArticle(paraMap.get("id"));
+    });
   }
-
 }
--- src/app/news/news-routing.module.ts
+++ src/app/news/news-routing.module.ts
@@ -1,33 +1,41 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { NewsPage } from './news.page';
+import { NewsPage } from "./news.page";
 
 const routes: Routes = [
   {
-    path: '',
-    component: NewsPage
+    path: "",
+    component: NewsPage,
   },
   {
-    path: 'all',
-    loadChildren: () => import('./all/all.module').then( m => m.AllPageModule)
+    path: "all",
+    loadChildren: () => import("./all/all.module").then((m) => m.AllPageModule),
   },
   {
-    path: 'tickets',
-    loadChildren: () => import('./tickets/tickets.module').then( m => m.TicketsPageModule)
+    path: "tickets",
+    loadChildren: () =>
+      import("./tickets/tickets.module").then((m) => m.TicketsPageModule),
   },
   {
-    path: 'club',
-    loadChildren: () => import('./club/club.module').then( m => m.ClubPageModule)
+    path: "club",
+    loadChildren: () =>
+      import("./club/club.module").then((m) => m.ClubPageModule),
   },
   {
-    path: 'junior-fans',
-    loadChildren: () => import('./junior-fans/junior-fans.module').then( m => m.JuniorFansPageModule)
+    path: "junior-fans",
+    loadChildren: () =>
+      import("./junior-fans/junior-fans.module").then(
+        (m) => m.JuniorFansPageModule
+      ),
   },
   {
-    path: 'news-detail',
-    loadChildren: () => import('./news-detail/news-detail.module').then( m => m.NewsDetailPageModule)
-  }
+    path: "news-detail/:id",
+    loadChildren: () =>
+      import("./news-detail/news-detail.module").then(
+        (m) => m.NewsDetailPageModule
+      ),
+  },
 ];
 
 @NgModule({
